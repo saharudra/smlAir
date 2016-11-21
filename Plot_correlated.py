165a0daadfa94085b2a6bd641af44510e8b1d08d
Sarthak Khanna <khannasarthak.1994@gmail.com>
2016-11-20 17:13:07 -0700

    Added Plot_correlated
    Reads correlation.csv and returns bar graph and a new csv file containig
    the top 50 geatures with highest correlation with country_destination.

--- Plot_correlated.py
+++ Plot_correlated.py
@@ -1,6 +1,16 @@
+# opens correlation.csv and creates top_50_features.csv and also plots the data
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('Correlation.csv', header=None)
-print (df)
+df = df.sort_values(1,ascending=False)
+top_50_features = df[1:51]
+top_50_features.columns = ['Feature Name','Correlation Value']
+top_50_features.plot.bar(color = 'g')
+print (top_50_features)
+top_50_features.to_csv('top_50_features.csv')
+plt.xlabel('Feature column number (numbering of final table)')
+plt.ylabel('Correlation Value')
+plt.show()
--- correlation.py
+++ correlation.py
@@ -1,3 +1,6 @@
+# DO NOT RUN, IT RUNS ON final_table.csv SIZE = 1.12 GB. final_table.csv is stored locally on Sarthak's system
+# Opens final_table.csv and creates correlation csv wrt destination country
+
 import pandas as pd
 
 df = pd.read_csv('final_table.csv')
